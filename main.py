f6e6f100a09afb9046d2f2e1f5222574b3e3ce77
Otieno <36030016+otienosteve@users.noreply.github.com>
2022-01-24 19:00:45 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+class today:
+  num=10
+
+  def getInput(name):
+    return name
+
+ts=today()
+
+greet='steve'
+print(ts.num)
+
+def getName(name):
+  return name
+title='otieno'
+print(getName('again'))
+
+arr=[1,2,3]
+s=0
+for i in range(10):
+  if i%2==0:
+    s+=i
+print(s) if i<10 else print(2)
