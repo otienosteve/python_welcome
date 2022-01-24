30de383f253e9cc9752d31fe8e97d1106d593249
Otieno <36030016+otienosteve@users.noreply.github.com>
2022-01-24 20:39:56 +0000

    loop

--- main.py
+++ main.py
@@ -1,22 +1,9 @@
-class today:
-  num=10
 
-  def getInput(name):
-    return name
-
-ts=today()
-
-greet='steve'
-print(ts.num)
-
-def getName(name):
-  return name
-title='otieno'
-print(getName('again'))
-
-arr=[1,2,3]
-s=0
-for i in range(10):
-  if i%2==0:
-    s+=i
-print(s) if i<10 else print(2)
+x=18
+y=15
+div=0
+for i in range(2,y):
+  if y%i==0 and x%i==0:
+    div=i
+    break
+print(div)
