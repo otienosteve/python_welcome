886743b9ae336201e9f6be67c37d1f20b9153462
Otieno <36030016+otienosteve@users.noreply.github.com>
2022-01-31 19:47:39 +0000

    get difference between lists in python

---
+++ diff.py
@@ -0,0 +1,8 @@
+#get values that are not duplicate in a list
+def getDiff():
+  ls=[1,2,3,4,5,6,7,9,1,2,3,5,6,7,8]
+  l3=[11,12,13,14]
+  l3.extend(l3)
+  diff=set(l3)-set(ls)
+  df=list(diff)
+  print(df)
--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import again
 import listcomp
 import file
+import diff
 
-name=file.Desc('Oliver',72,1999)
-#again.printer(7)
-#file.printName('steve')
-name.printChar()
+# name=file.Desc('Oliver',72,1999)
+# #again.printer(7)
+# #file.printName('steve')
+# name.printChar()
+# again.printer(5)
+# person2=file.Desc('Brenda',54,1999)
+# person2.printChar()
 
-person2=file.Desc('Brenda',54,1999)
-person2.printChar()
+# person3=file.Desc('Gilbert',32,1907)
+# person3.printChar()
+# l=['1','2','3','4','5','6','7','8','9','10','11']
+# listP=listcomp.findSum(l)
 
-person3=file.Desc('Gilbert',32,1907)
-person3.printChar()
-listcomp.findSum()
+# print(listP)
+diff.getDiff()
