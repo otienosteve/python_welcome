8af5ee4af162e4e5f6127cff7a5472a2d32a1389
Otieno <36030016+otienosteve@users.noreply.github.com>
2022-02-03 10:36:00 +0000

    charCode in python

--- main.py
+++ main.py
@@ -3,20 +3,21 @@ import listcomp
 import file
 import diff
 
-# name=file.Desc('Oliver',72,1999)
-# #again.printer(7)
-# #file.printName('steve')
-# name.printChar()
-# again.printer(5)
-# person2=file.Desc('Brenda',54,1999)
-# person2.printChar()
+
 f='Steve'
 print('My {} Name is {} {}'.format(f,'Otieno','Ranger'))
-print(f'welsome agin onboard Mr.{f}')
-# person3=file.Desc('Gilbert',32,1907)
-# person3.printChar()
-# l=['1','2','3','4','5','6','7','8','9','10','11']
-# listP=listcomp.findSum(l)
+print(f'welcome agin onboard Mr.{f}')
+name='steve'
+s=0
+char=[]
+for i in name:
+   s+=ord(i)
+   char.append(ord(i))
+print(s)
+print(char)
+#getcharcode in python
+str=''
+for t in char:
+  str+=chr(t)
 
-# print(listP)
-# diff.getDiff()
+print(str)
